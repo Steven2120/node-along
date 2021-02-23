284af86aa8d7269eee793ff85f0b748222dc5d85
Steven Moyano <stevenmoyano1@gmail.com>
2021-02-23 14:22:16 -0600

    completed task for 3 files

--- greet.js
+++ greet.js
@@ -1,1 +1,8 @@
 const getInput = require("./get-input.js");
+
+const userName = getInput();
+console.log("Hello " + userName);
+
+// Now when you run in node mental-age.js followed by something
+// to take in the place for the str then it returns "Hello + str"
+
--- mental-age.js
+++ mental-age.js
@@ -1,1 +1,17 @@
 const getInput = require("./get-input.js");
+
+let userAge = getInput();
+
+if (userAge === "--help") {
+    console.log("Please enter your age after 'node mental-age.js' and a space");
+}
+
+const mentalAge = userAge / 2 + 3;
+
+console.log("You are " + userAge + " years old.");
+
+console.log("Your mental age is " + mentalAge + " years old.");
+
+
+// let userAge = getInput();
+// console.log(userAge / 2 + 3);
